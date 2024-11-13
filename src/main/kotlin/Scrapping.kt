c078f05106de5a22c8c44f9ed987bac2d8db10dc
andre-carbajal <carbajalvargasandre@gmail.com>
2024-11-13 15:19:54 -0500

    scrapping improved

--- src/main/kotlin/Scrapping.kt
+++ src/main/kotlin/Scrapping.kt
@@ -8,40 +8,38 @@ import org.openqa.selenium.support.ui.ExpectedConditions
 import org.openqa.selenium.support.ui.WebDriverWait
 import java.time.Duration
 
-fun scrappingMifarma(url: String) {
+private fun scrapeWebsite(url: String, cssSelector: String) {
     val driver = EdgeDriver()
-    driver.get(url)
-
-    val wait = WebDriverWait(driver, Duration.ofSeconds(30))
-    wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div.col-xs-4.col-sm-2.col-md-6.col-lg-4.text-right.d-flex.align-items-center.justify-content-end.price-amount")))
-
-    val doc: Document = Jsoup.parse(driver.pageSource!!)
-
-    driver.quit()
-
     try {
-        val element = doc.select("div.col-xs-4.col-sm-2.col-md-6.col-lg-4.text-right.d-flex.align-items-center.justify-content-end.price-amount")[1]
-        println(element.text())
-    } catch (e: IndexOutOfBoundsException) {
-        println("Element not found")
+        driver.get(url)
+
+        val wait = WebDriverWait(driver, Duration.ofSeconds(30))
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(cssSelector)))
+
+        val doc: Document = Jsoup.parse(driver.pageSource!!)
+
+        val elementText = doc.select(cssSelector).getOrNull(1)?.text() ?: "There is not an offer"
+
+        if (elementText == "There is not an offer") {
+            println(elementText)
+        } else {
+            println("The price in the offer is: $elementText")
+        }
+    } finally {
+        driver.quit()
     }
 }
 
+fun scrappingMifarma(url: String) {
+    scrapeWebsite(
+        url,
+        "div.col-xs-4.col-sm-2.col-md-6.col-lg-4.text-right.d-flex.align-items-center.justify-content-end.price-amount"
+    )
+}
+
 fun scrappingInkaFarma(url: String) {
-    val driver = EdgeDriver()
-    driver.get(url)
-
-    val wait = WebDriverWait(driver, Duration.ofSeconds(30))
-    wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div.col-xs-5.col-sm-2.col-md-6.col-lg-4.text-right.d-flex.align-items-center.justify-content-end.price-amount")))
-
-    val doc: Document = Jsoup.parse(driver.pageSource!!)
-
-    driver.quit()
-
-    try {
-        val element = doc.select("div.col-xs-5.col-sm-2.col-md-6.col-lg-4.text-right.d-flex.align-items-center.justify-content-end.price-amount")[1]
-        println(element.text())
-    } catch (e: IndexOutOfBoundsException) {
-        println("Element not found")
-    }
+    scrapeWebsite(
+        url,
+        "div.col-xs-5.col-sm-2.col-md-6.col-lg-4.text-right.d-flex.align-items-center.justify-content-end.price-amount"
+    )
 }
